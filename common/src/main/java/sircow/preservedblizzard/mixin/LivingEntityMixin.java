84547219a02f239995c0731bbcfd5b4e6aae49d8
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2025-02-14 14:47:16 +0000

    0.0.4
    + prevent mob drops unless from player
    + add icon and description

---
+++ common/src/main/java/sircow/preservedblizzard/mixin/LivingEntityMixin.java
@@ -0,0 +1,23 @@
+package sircow.preservedblizzard.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    // make mob loot only drop when killed by a player
+    @Inject(method = "dropAllDeathLoot", at = @At("HEAD"), cancellable = true)
+    private void sir_cow$modifyMobDrops(ServerLevel level, DamageSource damageSource, CallbackInfo ci) {
+        if (damageSource.getEntity() instanceof Player) {
+            // nothing! continue as normal
+        } else {
+            ci.cancel();
+        }
+    }
+}
