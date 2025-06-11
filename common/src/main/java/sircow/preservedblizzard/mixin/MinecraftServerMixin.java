9634039506b56dd8123e88833cc4f95c1f8d3fa4
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2025-06-11 01:34:15 +0100

    0.6.1
    + world border changes

--- common/src/main/java/sircow/preservedblizzard/mixin/MinecraftServerMixin.java
+++ common/src/main/java/sircow/preservedblizzard/mixin/MinecraftServerMixin.java
@@ -1,7 +1,6 @@
 package sircow.preservedblizzard.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.progress.ChunkProgressListener;
 import net.minecraft.world.level.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,12 +8,11 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import sircow.preservedblizzard.other.IPlayerTimeData;
 import sircow.preservedblizzard.other.IWorldBorderStatus;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-    @Unique private static final int RATE_OF_CHANGE = 20 * 60 * 60;
+    @Unique private static final int TICKS_PER_HOUR = 20 * 60 * 60;
 
     @Inject(method = "createLevels", at = @At("TAIL"))
     private void preserved_blizzard$setWorldBorder(ChunkProgressListener listener, CallbackInfo ci) {
@@ -30,24 +28,40 @@ public class MinecraftServerMixin {
 
     @Inject(method = "tickServer", at = @At("TAIL"))
     private void preserved_blizzard$onServerTick(CallbackInfo ci) {
-        // Increase the world border size by 1 block
+        // increase the world border size by 1 block
         MinecraftServer server = (MinecraftServer) (Object) this;
         if (server.overworld() == null) {
             return;
         }
 
         WorldBorder overworldBorder = server.overworld().getWorldBorder();
-        if (overworldBorder.getSize() < 59999968) {
-            for (ServerPlayer player : server.getPlayerList().getPlayers()) {
-                IPlayerTimeData playerTimeData = (IPlayerTimeData) player;
-                playerTimeData.setOnlineTimeTicks(playerTimeData.getOnlineTimeTicks().get() + 1);
+        double currentDiameter = overworldBorder.getSize();
+        int onlinePlayers = server.getPlayerList().getPlayerCount();
+        double newSize = getNewSize(currentDiameter, onlinePlayers);
+        long lerpTime = 50L;
 
-                if (playerTimeData.getOnlineTimeTicks().get() >= RATE_OF_CHANGE) {
-                    double currentSize = overworldBorder.getSize();
-                    overworldBorder.setSize(currentSize + 2.0D);
-                    playerTimeData.setOnlineTimeTicks(playerTimeData.getOnlineTimeTicks().get() - RATE_OF_CHANGE);
-                }
-            }
+        if (currentDiameter >= 59999968.0D || onlinePlayers == 0) {
+            return;
         }
+
+        if (newSize > currentDiameter) {
+            overworldBorder.lerpSizeBetween(currentDiameter, newSize, lerpTime);
+        }
+        else {
+            overworldBorder.setSize(newSize);
+        }
+    }
+
+    @Unique
+    private static double getNewSize(double currentDiameter, int onlinePlayers) {
+        double ratePerHour = 60.0D / (Math.log(Math.pow(1.0D + 0.375D * currentDiameter, 2.25D)));
+        double finalRatePerHour = ratePerHour * onlinePlayers;
+        double increasePerTick = finalRatePerHour / TICKS_PER_HOUR;
+        double newSize = currentDiameter + increasePerTick;
+
+        if (newSize > 59999968.0D) {
+            newSize = 59999968.0D;
+        }
+        return newSize;
     }
 }
